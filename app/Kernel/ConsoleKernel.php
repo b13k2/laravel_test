4619ff1b466b88a9d349ff67abbee84fa1884f50
Alexandr Kokhan <fatalproject@gmail.com>
2023-11-05 01:04:08 +0300

    базовая настройка проекта

---
+++ app/Kernel/ConsoleKernel.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Kernel;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel;
+
+class ConsoleKernel extends Kernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands(): void
+    {
+        // $this->load(__DIR__.'/Commands');
+        // require base_path('routes/console.php');
+    }
+}
